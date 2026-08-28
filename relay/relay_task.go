6fd9b22864929f395f618c8d3000b9e8f999ee6b
ZhengJin <fangzhengjin@gmail.com>
2026-08-29 02:07:35 +0800

    fix(relay): 固定模型映射为单层语义

--- relay/relay_task.go
+++ relay/relay_task.go
@@ -224,7 +224,7 @@ func RelayTaskSubmit(c *gin.Context, info *relaycommon.RelayInfo) (*TaskSubmitRe
 	mappedBeforeValidate := info.OriginModelName != ""
 	if mappedBeforeValidate {
 		info.UpstreamModelName = info.OriginModelName
-		if err := helper.ModelMappedHelper(c, info, nil); err != nil {
+		if err := helper.TaskModelMappedHelper(c, info, nil); err != nil {
 			return nil, service.TaskErrorWrapperLocal(err, "model_mapping_failed", http.StatusBadRequest)
 		}
 	}
@@ -241,7 +241,7 @@ func RelayTaskSubmit(c *gin.Context, info *relaycommon.RelayInfo) (*TaskSubmitRe
 	if !mappedBeforeValidate {
 		info.OriginModelName = modelName
 		info.UpstreamModelName = modelName
-		if err := helper.ModelMappedHelper(c, info, nil); err != nil {
+		if err := helper.TaskModelMappedHelper(c, info, nil); err != nil {
 			return nil, service.TaskErrorWrapperLocal(err, "model_mapping_failed", http.StatusBadRequest)
 		}
 	}
